1834381d24103db199a56bd1424f51e9be304f28
Polokurse <j21marda@student.his.se>
2024-04-25 14:04:45 +0200

    skapat Mountain klass med 2 constructors

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,24 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain ()
+    {
+        name="inget namn";
+        location="ingen plats";
+        height=-1;
+    }
+
+    public Mountain(String n, String l, int h)
+    {
+        name=n;
+        location=l;
+        height=h;
+    }
+}
+
+
+
